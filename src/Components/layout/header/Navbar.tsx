ea4369e85b6bba6daed06304c67cbb6965765f39
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-01-11 16:31:48 +0200

    4

--- src/Components/layout/header/Navbar.tsx
+++ src/Components/layout/header/Navbar.tsx
@@ -13,38 +13,40 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <header className="bg-gray-900">
-      <div className="max-w-screen-xl px-4 mx-auto w-full">
-        <div className="flex flex-row items-center w-full h-16">
-          <button className="mr-6">
-            <MenuIcon
-              sx={{
-                width: 48,
-                height: 48,
-                color: "#ffffff",
-              }}
-            />
-          </button>
-          <Link href="/" className="mr-6">
-            <Image
-              src={navLogo}
-              alt="logo"
-              width={30}
-              height={32}
-              priority={true}
-              className="max-w-[30px] "
-            />
-          </Link>
-          <SearchField />
-          <Link href={`${pathname}?shopCard=true`}>
-            <ShoppingCartOutlinedIcon
-              fontSize="large"
-              sx={{ color: "#ffffff" }}
-            />
-          </Link>
+    <div className="bg-gray-900">
+      <header>
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex flex-row items-center ">
+            <button className="mr-6">
+              <MenuIcon
+                sx={{
+                  width: 48,
+                  height: 48,
+                  color: "#ffffff",
+                }}
+              />
+            </button>
+            <Link href="/" className="mr-6">
+              <Image
+                src={navLogo}
+                alt="logo"
+                width={30}
+                height={32}
+                priority={true}
+                style={{maxWidth: '30px'}}
+              />
+            </Link>
+            <SearchField />
+            <Link href={`${pathname}?shopCard=true`}>
+              <ShoppingCartOutlinedIcon
+                fontSize="large"
+                sx={{ color: "#ffffff" }}
+              />
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
