cfe084674e5b256eb025c06709df03c81d9bec74
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-01-11 18:48:28 +0200

    8

--- src/Components/layout/header/Navbar.tsx
+++ src/Components/layout/header/Navbar.tsx
@@ -8,48 +8,45 @@ import SearchField from "./SearchField";
 import Link from "next/link";
 import Image from "next/image";
 
+import style from "./style.module.css";
+
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
   return (
-      <header>
-        <div style={{
-          padding: '16px 8px',
-          backgroundColor: '#262626'
-        }} className="max-w-screen-xl px-4 py-2 mx-auto bg-gray-900">
-          <div style={{
-            gap: '24px'
-          }} className="flex flex-row items-center gap-6">
-            <button>
-              <MenuIcon
-                sx={{
-                  width: 48,
-                  height: 48,
-                  color: "#ffffff",
-                }}
-              />
-            </button>
-            <Link href="/">
-              <Image
-                src={navLogo}
-                alt="logo"
-                width={30}
-                height={32}
-                priority={true}
-                style={{maxWidth: '30px'}}
-              />
-            </Link>
-            <SearchField />
-            <Link href={`${pathname}?shopCard=true`}>
-              <ShoppingCartOutlinedIcon
-                fontSize="large"
-                sx={{ color: "#ffffff" }}
-              />
-            </Link>
-          </div>
+    <header>
+      <div className={style.header}>
+        <div className={style.headerWrapper}>
+          <button>
+            <MenuIcon
+              sx={{
+                width: 48,
+                height: 48,
+                color: "#ffffff",
+              }}
+            />
+          </button>
+          <Link href="/">
+            <Image
+              src={navLogo}
+              alt="logo"
+              width={30}
+              height={32}
+              priority={true}
+              style={{ maxWidth: "30px" }}
+            />
+          </Link>
+          <SearchField />
+          <Link href={`${pathname}?shopCard=true`}>
+            <ShoppingCartOutlinedIcon
+              fontSize="large"
+              sx={{ color: "#ffffff" }}
+            />
+          </Link>
         </div>
-      </header>
+      </div>
+    </header>
   );
 };
 
