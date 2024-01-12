825e45bc0e96f1f94d50634379bc8f635e3f8671
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-01-12 12:04:21 +0200

    10

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import navLogo from "../../../../public/navLogo.svg";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import SearchField from "@/Components/layout/header/SearchField";
+
+const page = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  const pathname = usePathname();
+
+  return (
+    <header className="bg-[#262626]">
+      <div className="flex justify-between gap-6 items-center px-4 py-2 mx-auto max-w-screen-2xl">
+      <button onClick={handleNav}>
+        <MenuIcon
+          sx={{
+            width: 48,
+            height: 48,
+            color: "#ffffff",
+          }}
+        />
+      </button>
+      <Link href="/">
+        <Image
+          src={navLogo}
+          alt="logo"
+          width={30}
+          height={32}
+          priority={true}
+          className="max-w-[30px] "
+        />
+      </Link>
+      <SearchField />
+      <Link href={`${pathname}?shopCard=true`}>
+        <ShoppingCartOutlinedIcon fontSize="large" className="text-white" />
+      </Link>
+      </div>
+    </header>
+  );
+};
+
+export default page;
