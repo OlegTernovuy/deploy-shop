e6fd75ea2711641b408000e77fd86ea6e6dcca7d
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-01-12 13:58:22 +0200

    18

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import Button from "@mui/material/Button";
 
-import buttonIcon from "../../../../public/buttonIcon.svg";
+import buttonIcon from "../../../public/buttonIcon.svg";
 
 const MainButton = () => {
   return (
